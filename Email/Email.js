7617b96e81b15d264f11d38df9704b37a42551f6
Miracle0312 <prajapatidhani06@gmail.com>
2025-03-06 11:39:37 +0530

    [Dhani] Add. UC01 Validate email with some mandatory parts

---
+++ Email/Email.js
@@ -0,0 +1,7 @@
+ //UC02- regex to ensure @ and bridgelabz 
+function validateEmail(email) {
+    const regex = /^abc\.[a-zA-Z0-9._%+-]+@bridgelabz\.co(\.[a-zA-Z0-9.-]+)?$/;
+    return regex.test(email);
+}
+const email = "abc.xyz@bridgelabz.co.in";
+console.log(validateEmail(email));
