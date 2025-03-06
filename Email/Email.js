af6c95e4163efbd803be03734e6499cd1c2dbd24
Miracle0312 <prajapatidhani06@gmail.com>
2025-03-06 12:00:50 +0530

    [Dhani] Add. UC04 handle optional part in Email

--- Email/Email.js
+++ Email/Email.js
@@ -1,7 +1,16 @@
- //UC02- regex to ensure @ and bridgelabz
+ //UC04- to handle optional part
 function validateEmail(email) {
-    const regex = /^abc\.[a-zA-Z0-9._%+-]+@bridgelabz\.co(\.[a-zA-Z0-9.-]+)?$/;
+    const regex = /^abc(\.[a-zA-Z0-9._+-]+)?@bridgelabz\.co(\.[a-zA-Z0-9.-]+)?$/;
     return regex.test(email);
 }
-const email = "abc.xyz@bridgelabz.co.in";
-console.log(validateEmail(email));
+const email1 = "abc.xyz@bridgelabz.co.in";
+const email2 = "abc_123@bridgelabz.co.in";
+const email3 = "abc+test@bridgelabz.co.in";
+const email4 = "abc-xyz@bridgelabz.co.in";
+const email5 = "abc.invalid@bridgelabz.co.in";
+
+console.log(validateEmail(email1)); 
+console.log(validateEmail(email2)); 
+console.log(validateEmail(email3)); 
+console.log(validateEmail(email4)); 
+console.log(validateEmail(email5));
