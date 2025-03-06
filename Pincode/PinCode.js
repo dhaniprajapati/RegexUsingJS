32e2216d530ba4aaff765538808dfca6d0983775
Miracle0312 <prajapatidhani06@gmail.com>
2025-03-06 11:18:35 +0530

    [Dhani] Add. UC02 restrict taking alphabates or special characters

--- Pincode/PinCode.js
+++ Pincode/PinCode.js
@@ -3,5 +3,8 @@ function isValidPin(pin){
     const regex= /^[1-9][0-9]{5}$/;
     return regex.test(pin);
 }
-const pin="400088";
+const ValidPin="400088";
+console.log(isValidPin(ValidPin));
+//UC_2- restrict the pin for taking alphabates
+const pin = 'A400088';
 console.log(isValidPin(pin));
